8e86826250f259c6281b4b44022b1c25b7137034
amvilly <athenamvillard@gmail.com>
2024-07-09 03:14:59 -0400

    revisions debug attempts

--- api/notion.js
+++ api/notion.js
@@ -16,6 +16,7 @@ module.exports = async (req, res) => {
 
     res.status(200).json(tasks);
   } catch (error) {
+    console.error('Error:', error);
     res.status(500).json({ error: 'Failed to fetch tasks' });
   }
 };
