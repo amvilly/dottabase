a7629d89d35c8f0e67516400b037516edc21ba6a
amvilly <athenamvillard@gmail.com>
2024-07-09 20:05:42 -0400

    edits debug

--- api/notion.js
+++ api/notion.js
@@ -4,9 +4,11 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 module.exports = async (req, res) => {
   try {
+    console.log('Fetching data from Notion...');
     const response = await notion.databases.query({
       database_id: process.env.NOTION_DATABASE_ID,
     });
+    console.log('Notion response received:', JSON.stringify(response, null, 2));
     
     const tasks = response.results.map(page => ({
       name: page.properties.Name.title[0].plain_text,
@@ -15,9 +17,10 @@ module.exports = async (req, res) => {
       lastCompleted: page.properties['Last completed'].date.start,
     }));
 
+    console.log('Sending response:', JSON.stringify(tasks, null, 2));
     res.status(200).json(tasks);
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error in Notion API:', error);
     res.status(500).json({ error: error.message });
   }
 };
