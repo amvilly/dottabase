b55c23923e6db16e30e0a22f2bf682d538f7fc92
amvilly <athenamvillard@gmail.com>
2024-07-10 20:54:54 -0400

    trying

--- testNotion.js
+++ testNotion.js
@@ -1,7 +1,7 @@
 const { Client } = require('@notionhq/client');
 
 // Initialize Notion client
-const notion = new Client({ auth: 'secret_ahKhJkltlXAqn6VIr2g15YDouc7rimVcmUHuZn359hN' });
+const notion = new Client({ auth: 'secret_yi4aZxCF2fro9eQIMwXm8V3aYQssPWmxcEtAlgDQ2t4' });
 
 async function fetchDatabases() {
     try {
